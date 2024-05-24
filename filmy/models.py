affae828ee0b960b4fc7e2dff8b8527564272b2d
František Pešula <frantiek.peula@student.gyarab.cz>
2024-05-24 09:03:17 +0200

    models edited

--- filmy/models.py
+++ filmy/models.py
@@ -6,9 +6,15 @@ class Movie(models.Model):
     footage = models.PositiveSmallIntegerField(blank=True, null=True, help_text="pls in minutes")
     description = models.TextField(blank=True)
     main_picture = models.ImageField(blank=True, null=True)
+    director = models.ForeignKey('Director', blank=True, null=True, on_delete=models.SET_NULL)
+    actors = models.ManyToManyField('Actor', blank=True)
+    genres = models.ManyToManyField('Genre', blank=True)
 
     def __str__(self):
         return self.title
+        
+    def genres_display(self):
+        return ", ".join([i.name for i in self.genres.all()])
 
 class Director(models.Model):
     title = models.CharField(max_length=300)
